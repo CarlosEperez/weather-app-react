331887d4de6c4f277217e6db1fb2f2343e7df7d4
carloseperez <netrizx38@gmail.com>
2022-05-24 22:49:23 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -5,11 +5,29 @@ const api = {
 }
 
 function App() {
+  
+  const dateBuilder = (date) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let myDay = days[date.getDay()];
+    let myDate = date.getDate();
+    let myMonth = months[date.getMonth()];
+    let myYear = date.getFullYear();
+    return `${myDay} ${myDate} ${myMonth} ${myYear}`
+  }
+
+
   return (
     <div className="app warm">
       <main>
         <div className="search-box">
-          <input type="text" className="search-bar" placeholder="search..."></input>
+          <input type="text" className="search-bar" placeholder="search..." />
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+
         </div>
       </main>
     </div>
